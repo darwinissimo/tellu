a5a1da671b11592f64e7176b9d932bd055bf481a
spannerbyte <lg.dantas1975@gmail.com>
2021-12-08 00:51:52 -0300

    LogIn - set up

--- app/src/main/java/com/spanner/tellu/LogIn.kt
+++ app/src/main/java/com/spanner/tellu/LogIn.kt
@@ -2,10 +2,30 @@ package com.spanner.tellu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+import javax.security.auth.login.LoginException
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Itent(this, SignUp::class.java) // evento na classe SignUp
+            startActivity(intent)
+        }
     }
 }
