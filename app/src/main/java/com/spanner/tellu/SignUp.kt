af32dd3795b6d19ead7a1266ecd3872c6016853f
spannerbyte <lg.dantas1975@gmail.com>
2021-12-11 03:39:32 -0300

    SignUp - New Users

--- app/src/main/java/com/spanner/tellu/SignUp.kt
+++ app/src/main/java/com/spanner/tellu/SignUp.kt
@@ -1,9 +1,11 @@
 package com.spanner.tellu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
@@ -35,5 +37,18 @@ class SignUp : AppCompatActivity() {
 
     private fun signUp(email: String, password: String) {
         // logic of creat user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for jumping to home
+
+                    val intent = Intent(this@SignUp, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@SignUp, "Ocorreu um erro", Toast.LENGTH_SHORT).show()
+
+                }
+            }
     }
 }
