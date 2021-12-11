d2745784291f15280a4afb45f9bb5aaf8fb8f122
spannerbyte <lg.dantas1975@gmail.com>
2021-12-11 19:08:11 -0300

    Chat Ac

--- app/src/main/java/com/spanner/tellu/ChatActivity.kt
+++ app/src/main/java/com/spanner/tellu/ChatActivity.kt
@@ -2,10 +2,22 @@ package com.spanner.tellu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageBox: EditText
+    private lateinit var sendButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        chatRecyclerView = findViewById(R.id.chatRecyclerView)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sentButton)
     }
 }
